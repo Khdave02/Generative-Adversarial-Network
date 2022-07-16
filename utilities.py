5fda8bde04238e970db3bbdbc319875f8f5c5ac1
Khdave02 <87975841+Khdave02@users.noreply.github.com>
2022-07-16 10:09:22 +0530

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -8,13 +8,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 
 
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
 
 
 def view_images(img_list,real_batch,epoch):
